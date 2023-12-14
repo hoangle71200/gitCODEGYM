8b0aa46a3587ac043bacdbf2a10be2e049e320cb
hoangle71200 <108779064+hoangle71200@users.noreply.github.com>
2023-12-14 14:47:48 +0700

    mycodegym

---
+++ front-end/js/hienthidaura.js
@@ -0,0 +1,18 @@
+
+//B1
+// function showMessage(){
+//     alert('Xin chào!');
+// }
+
+// //B2
+// function showMessage(){
+//     document.getElementById('messageDiv').innerHTML = 'Xin chào!';
+// }
+// //B3
+// function showMessage(){
+//     document.write('Xin chào!');
+// }
+// B4
+function showMessage(){
+    console.log('Xin chào!');
+}
